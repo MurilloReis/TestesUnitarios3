7581f2c729f6c67367d0a3e94fa8083b3ae9f305
Murillo Reis <133406093+MurilloReis@users.noreply.github.com>
2025-10-20 21:40:11 -0300

    Update aluno.py

--- aluno.py
+++ aluno.py
@@ -1,11 +1,20 @@
 class AlunoClass:
-  def __init__(self, nome, sobrenome, nota):    
-    self.nome = nome;
-    self.sobrenome = sobrenome;
-    self.nota = nota;
+
+  def __init__(self, nome, sobrenome, nota):
+    self.nome = nome
+    self.sobrenome = sobrenome
+    self.nota = nota
 
   def mostrarAluno(self):
-    return 'Aluno: ' + self.nome + ' ' + self.sobrenome + ' - Nota: ' + str(self.nota);    
+    return 'Aluno: ' + self.nome + ' ' + self.sobrenome + ' - Nota: ' + str(
+        self.nota)
 
-  def salvar(self, conexao, colecao):    
-    print('Escreva o seu método aqui...')
+  def salvar(self, conexao, colecao):
+    colecao_db = conexao[colecao]
+    aluno_data = {
+        "nome": self.nome,
+        "sobrenome": self.sobrenome,
+        "nota": self.nota
+    }
+    resultado = colecao_db.insert_one(aluno_data)
+    return resultado.acknowledged
